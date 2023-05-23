e644260dcacbff1165365ef6a7002eb640f7f45a
Omotolani Akinmola <akinmolaomotolani@gmail.com>
2023-05-23 18:17:31 +0100

    made corections on review

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-"use strict ";
+'use strict ';
 
-const btnMobileNav = document.querySelector(".btn-mobile-nav");
-const headerNav = document.querySelector(".header");
-btnMobileNav = addEventListener("click", function () {
-  headerNav.classList.toggle("nav-open");
+const btnMobileNav = document.querySelector('.btn-mobile-nav');
+const headerNav = document.querySelector('.header');
+btnMobileNav = addEventListener('click', () => {
+  headerNav.classList.toggle('nav-open');
 });
