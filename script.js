8f629236ee568e00068d4589fa422d4e7ff7e2be
anold <anoldmwawasi@gmail.com>
2023-05-23 11:55:33 +0300

    added functionality to the nav mobile menu

--- script.js
+++ script.js
@@ -2,3 +2,6 @@
 
 const btnMobileNav = document.querySelector(".btn-mobile-nav");
 const headerNav = document.querySelector(".header");
+btnMobileNav = addEventListener("click", function () {
+  headerNav.classList.toggle("nav-open");
+});
