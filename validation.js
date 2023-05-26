4c419ccd38e1e07d7646169a55b5105e4092ef59
Omotolani Akinmola <akinmolaomotolani@gmail.com>
2023-05-26 10:24:57 +0100

    Email validation with js

---
+++ validation.js
@@ -0,0 +1,5 @@
+const error = document.getElementById('error');
+const submit = document.getElementById('submit-btn');
+error.style.color = 'red';
+error.style.textAlign = 'center';
+error.style.fontSize = '16px';
